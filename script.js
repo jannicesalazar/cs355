c447b6a337223c10ca1ca4a87c265b1fe5edaee2
jannicesalazar <jannicesalazars@gmail.com>
2024-02-12 15:29:17 -0500

    hi

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function myfunction() {
+const $ = document.querySelector.bind(document);
+
+function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-const $ = document.querySelector.bind(document);
 
-$('#darkBtn').addEventListener('click',toggleDark)
 
-function toggleDark(){
 
-    if($(':root').hasAttribute('dark-mode')){
-        $(':root').removeAttribute('dark-mode');
-    }else{
-        $(':root').setAttribute('dark-mode',true);
-    }
-}
