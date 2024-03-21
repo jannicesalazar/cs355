7a5bf49a0644d07e3423748c08a6700a36c71c2a
jannicesalazar <jannicesalazars@gmail.com>
2024-03-20 21:59:44 -0400

    hi

--- asg1,2/script.js
+++ asg1,2/script.js
@@ -1,9 +1,26 @@
 'use strict'
 
-const $ = document.querySelector.bind(document);
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+function darkmode(){
+    var SetTheme = document.body;
+    SetTheme.classList.toggle("dark-mode")
+    var theme;
+    if(SetTheme.classList.contains("dark-mode")){
+        console.log("Dark mode");
+        theme = "DARK";
+    }else{
+        console.log("Light mode");
+        theme = "LIGHT";
+    }
+    // save to localStorage
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
 }
 
+setInterval(() => {
+    let GetTheme = JSON.parse(localStorage.getItem("PageTheme")); // get from localStorage
+    console.log(GetTheme);
+    if (GetTheme === "DARK") { 
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+}, 5);
